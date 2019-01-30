d7e9bcf80f4709c315481d912713ccdc88d27a03
Andrey Mykhaylov <andrey.mykhaylov@dataart.com>
2019-01-30 21:12:47 +0200

    'flow3'

--- flow.js
+++ flow.js
@@ -1,10 +1,28 @@
+const {distance_object} = require('./vehicle-ride');
 const {readInputFile, writeOutputFile} = require('./input-output');
+const createKDTree = require("static-kdtree");
 
 // const files = ['a_example.in', 'b_should_be_easy.in', 'c_no_hurry.in', 'd_metropolis.in', 'e_high_bonus.in'];
 const fileName = 'b_should_be_easy.in';
 
 readInputFile(fileName, (response) => {
     let {rides, vehicleCount, bonus, steps} = response;
+
+    let flowRides = rides.map((ride, rideIndex) => ({...ride, rideIndex, flow: 0}));
+    const departures = rides.map((ride) => ride.intersections[0]);
+    const kdtree = createKDTree(departures);
+    flowRides = flowRides.map((flowRide) => {
+        const flowArray = [];
+        kdtree.rnn(flowRide.intersections[1], 20, (point) => {
+                if(point !== undefined) {
+                    flowArray.push(point);
+                }
+                else{
+                    flowRide.flow = flowArray.length;
+                }
+            });
+        return flowRide;
+    })
 
     // writeOutputFile(fileName, vehicleRides, originalRides, (writeResult) => {
     //     console.log(writeResult);
--- vehicle-ride.js
+++ vehicle-ride.js
@@ -5,27 +5,32 @@ const { max } = require('lodash');
 //  step: number
 // }
 
-export const distance_to_ride_start = (start, end) =>
+ const distance_to_ride_start = (start, end) =>
     Math.abs(start[0] - end[0]) + Math.abs(start[1] - end[1]);
 
-export const wait_time = (vehicleRide, ride) =>
+ const distance_object = (start, end) =>
+    Math.abs(start.x - end.x) + Math.abs(start.y - end.y);
+
+ const wait_time = (vehicleRide, ride) =>
     max(0, ride.earliestStart - (vehicleRide.step + distance_to_ride_start(vehicleRide.ride.intersections[1], ride.intersections[0])));
 
-export const arrival = (vehicleRide, ride) =>
+ const arrival = (vehicleRide, ride) =>
     vehicleRide.step
     + distance_to_ride_start(vehicleRide.ride.intersections[1], ride.intersections[0])
     + wait_time(vehicleRide, ride)
     + distance_to_ride_start(ride.intersections[0], ride.intersections[1]);
 
-export const can_start_on_time = (vehicleRide, ride) =>
+ const can_start_on_time = (vehicleRide, ride) =>
     vehicleRide.step + distance_to_ride_start(vehicleRide, ride) <= ride.earliestStart;
 
-export const can_finish_in_time = (vehicleRide, ride, steps) =>
+ const can_finish_in_time = (vehicleRide, ride, steps) =>
     !!arrival(vehicleRide, ride) <= Math.min(ride.latestFinish, steps);
 
-export const assignRide = (vehicleRide, ride) => {
+ const assignRide = (vehicleRide, ride) => {
     // self.assigned_rides.append(ride.rid)
-    const step_departure = max(ride.earliestStart, vehicleRide.step + distance_to_ride_start(vehicleRide, ride))
+    const step_departure = Math.max(ride.earliestStart, vehicleRide.step + distance_to_ride_start(vehicleRide, ride))
     vehicleRide.step = step_departure + distance_to_ride_start(ride.intersections[0], ride.intersections[1])
     vehicleRide.ride = ride;
-}
+};
+
+module.exports = {distance_object};
